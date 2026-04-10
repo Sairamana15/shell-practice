05ba8a70179fc9523e542856dc25f27ae7d52acd
Sairamana15 <sairamana824785@gmail.com>
2026-04-10 11:07:07 +0530

    some messages

---
+++ shell-scripting/03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=ramesh
+person2=suresh
+
+echo "$person1:: Hi $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you?"
+echo "$person1:: What are you learning recently?"
+echo "$person2:: I am learning Shell scripting $person1"
