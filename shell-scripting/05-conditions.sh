2c54bdb6b3994ede64a8f62335873b1907699868
Sairamana15 <sairamana824785@gmail.com>
2026-04-11 12:21:25 +0530

    some messages

--- shell-scripting/05-conditions.sh
+++ shell-scripting/05-conditions.sh
@@ -3,6 +3,8 @@ number=$1
 
 if [ $number -lt 10 ]; then
     echo "given number $number is less than 10"
+elif[ $number -eq 10 ]; then
+    echo "given number $number is equal to 10"
 else
-    echo "given number $number is greater than or equal to 10"
+    echo "given number $number is greater than 10"
 fi
